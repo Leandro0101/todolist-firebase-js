19ee6131a2a7f23202ce0c15a06942d028265037
Leandro0101 <leandrovieira3306@gmail.com>
2020-11-01 22:22:33 -0300

    authentication with email and password, email confirmation

---
+++ js/auth.js
@@ -0,0 +1,48 @@
+authForm.onsubmit = function (event) {
+    showItem(loading)
+    event.preventDefault()
+    if (authForm.submitAuthForm.innerHTML == 'Acessar') {
+        firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
+            console.log('Falha no acesso')
+            console.log(error)
+            hideItem(loading)
+        })
+    } else {
+        firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
+            console.log('Falha no cadastro')
+            console.log(error)
+            hideItem(loading)
+        })
+    }
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    hideItem(loading)
+    if (user) {
+        showUserContent(user)
+    } else {
+        showAuth()
+    }
+})
+// Permite o user a sair da conta dele
+function signOut() {
+    firebase.auth().signOut().catch(function (error) {
+        console.log('Falha ao sair da conta')
+        console.log(error)
+    })
+}
+
+function sendEmailVerification() {
+    showItem(loading)
+    const user = firebase.auth().currentUser
+    user.sendEmailVerification().then(function () {
+        alert(`email de verificação foi enviado para ${user.email}! Acesse a sua caixa de entrada`)
+    }).catch(function (error) {
+        alert('Houve uma erro ao enviar o email de verificação')
+        console.log(error)
+    }).finally(function(){
+        hideItem(loading)
+    })
+}
+
+
---
+++ js/utils.js
@@ -0,0 +1,58 @@
+// Defindo referências para elementos da página
+const authForm = document.getElementById('authForm')
+const authFormTitle = document.getElementById('authFormTitle')
+const register = document.getElementById('register')
+const access = document.getElementById('access')
+const loading = document.getElementById('loading')
+const auth = document.getElementById('auth')
+const userContent = document.getElementById('userContent')
+const userEmail = document.getElementById('userEmail')
+const sendEmailVerificationDiv = document.getElementById('sendEmailVerificationDiv')
+const emailVerified = document.getElementById('emailVerified')
+
+// Alterar o formulário de autenticação para o cadastro de novas contas
+function toggleToRegister() {
+  authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
+  authFormTitle.innerHTML = 'Insira seus dados para se cadastrar'
+  hideItem(register)
+  showItem(access)
+}
+
+// Alterar o formulário de autenticação para o acesso de contas já existentes
+function toggleToAccess() {
+  authForm.submitAuthForm.innerHTML = 'Acessar'
+  authFormTitle.innerHTML = 'Acesse a sua conta para continuar'
+  hideItem(access)
+  showItem(register)
+}
+
+// Simpplifica a exibição de elementos da página
+function showItem(element) {
+  element.style.display = 'block'
+}
+
+// Simpplifica a remoção de elementos da página
+function hideItem(element) {
+  element.style.display = 'none'
+}
+
+function showUserContent(user){
+    console.log(user)
+    if(user.emailVerified){
+        emailVerified.innerHTML = 'Email verificado'
+        hideItem(sendEmailVerificationDiv)
+    }else{
+        emailVerified.innerHTML = 'Email não verificado'
+        showItem(sendEmailVerificationDiv)
+    }
+    userEmail.innerHTML = user.email
+    hideItem(auth)
+    showItem(userContent)
+}
+
+function showAuth(){
+    authForm.email.value = ''
+    authForm.password.value = ''
+    hideItem(userContent)
+    showItem(auth)
+}
